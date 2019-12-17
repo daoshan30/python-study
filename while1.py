b1fbebbe86de5f4fd64306e73e0245802827bb4f
Daoshan30 <daoshan30@gmail.com>
2019-12-17 14:36:58 +0800

    study while loop

---
+++ while1.py
@@ -0,0 +1,10 @@
+while True:#always run
+	mode = input('please input game mode: ')
+	if mode == 'q':#stop condition
+	    break# quit
+	elif mode == '1':
+	    print('start mode 1')
+	elif mode == '2':
+		print('start mode 2')
+	else:
+		print('just can input 1/2/q')
